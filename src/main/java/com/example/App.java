24ede11ab254e53bfeb3ce621f680d81fe972f79
parth0285 <parthbole02@gmail.com>
2026-02-09 15:25:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,11 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
     }
 }
